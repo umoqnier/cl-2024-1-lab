643e640e48d92a8ee3d8cd4a8b4341bd673a8580
LeninPA <lenin.pavon@comunidad.unam.mx>
2023-09-30 16:59:23 -0600

    FEAT: n-grams nahuatl

--- LeninPavonAlvarez/p04-zipf/p04-zipf.py
+++ LeninPavonAlvarez/p04-zipf/p04-zipf.py
@@ -94,23 +94,40 @@ def plot_corpus_tag(tags, show = False, with_labels = False):
         plt.plot(x,y)
         plt.show()
     return x,y
-n=100  
+# Código ayudantía
+def extract_words_from_sentence(sentence: str) -> list:
+    return sub(r'[^\w\s\']', ' ', sentence).lower().split()
 
-brown_char = sort_dict(process_tag_corpus(brown.tagged_words()))
-cess_esp_char = sort_dict(process_tag_corpus(cess_esp.tagged_words()))
-tags_brown = get_frequencies_dict(brown_char,n)
-tags_cess_esp = get_frequencies_dict(cess_esp_char,n)
-brown_x, brown_y = plot_corpus_tag(tags_brown)
-cess_esp_x, cess_esp_y = plot_corpus_tag(tags_cess_esp)
-plt.plot(brown_x, brown_y)
-plt.plot(cess_esp_x, cess_esp_y)
-plt.show()
+def preprocess_corpus_words(corpus):
+    # Obtener la oración de L1,
+    # quitar signos de puntuación y
+    # obtiene la lista de palabras
+    word_list_l1 = []
+    word_list_l2 = []
+    for row in corpus:
+        word_list_l1.extend(extract_words_from_sentence(row[0]))
+    # Obtener la oración de L1,
+    # quitar signos de puntuación y
+    # obtiene la lista de palabras
+        word_list_l2.extend(extract_words_from_sentence(row[1]))
+    return word_list_l1, word_list_l2
+# n=100  
+
+# brown_char = sort_dict(process_tag_corpus(brown.tagged_words()))
+# cess_esp_char = sort_dict(process_tag_corpus(cess_esp.tagged_words()))
+# tags_brown = get_frequencies_dict(brown_char,n)
+# tags_cess_esp = get_frequencies_dict(cess_esp_char,n)
+# brown_x, brown_y = plot_corpus_tag(tags_brown)
+# cess_esp_x, cess_esp_y = plot_corpus_tag(tags_cess_esp)
+# plt.plot(brown_x, brown_y)
+# plt.plot(cess_esp_x, cess_esp_y)
+# plt.show()
 """
 Parte 2
 """
 # # Importar corpus de nahúatl, otomí y español
-# axolotl = elotl_corpus.load("axolotl")
-# tsunkua = elotl_corpus.load("tsunkua")
+axolotl = elotl_corpus.load("axolotl")
+tsunkua = elotl_corpus.load("tsunkua")
 # # Pre procesamiento de corpus
 # spanish_char_na, nahuatl_char = preprocess_corpus(axolotl)
 # spanish_char_oto, otomi_char = preprocess_corpus(tsunkua)
@@ -118,8 +135,39 @@ Parte 2
 # plt.plot(range(len(D)), list(D.values()))
 # plt.xticks(range(len(D)), list(D.keys()))
 # plt.show()
+"""
+Parte 3
+"""
+spanish_words_na, nahuatl_words = preprocess_corpus_words(axolotl)
+spanish_words_oto, otomi_words = preprocess_corpus_words(tsunkua)
+
+def get_ngram(corpus,n=2)->list:
+    count = 0
+    ngrams = []
+    current = ""
+    for word in corpus:
+        for char in word:
+            count += 1
+            current += char
+            if count == n:
+                ngrams.append(current)
+                current = ""
+                count = 0
+    return ngrams
+
+def plot_frequencies(frequencies: list, title="Freq of words"):
+    x = list(range(1, len(frequencies)+1))
+    plt.plot(x, frequencies, "-v")
+    plt.xlabel("Freq rank (r)")
+    plt.ylabel("Freq (f)")
+    plt.title(title)
 
 
+most_common_count = 100
+nahuatl_ngram = Counter(get_ngram(nahuatl_words))
+nahuatl_ngram_freqs = get_frequencies(nahuatl_ngram, most_common_count)
+plot_frequencies(nahuatl_ngram_freqs, f"Frequencies for Nahúatl {most_common_count} most common")
+plt.show()
 # Importar n-gramas de caracteres n=2
 # Respuestas stopwords coinciden con las paabras más comunes en Zipf?
 # Realizar nube de palabras sans stopwords et palabras de zipf
