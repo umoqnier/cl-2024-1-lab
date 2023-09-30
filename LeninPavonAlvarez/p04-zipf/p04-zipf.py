af64521824d6389b5cca1b11c25a8f862bf3158e
LeninPA <lenin.pavon@comunidad.unam.mx>
2023-09-30 15:14:09 -0600

    FEAT: Procesamiento nahuatl otomi

---
+++ LeninPavonAlvarez/p04-zipf/p04-zipf.py
@@ -0,0 +1,85 @@
+# Bibliotecas
+from collections import Counter,defaultdict
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = [15, 6]
+from re import sub
+import numpy as np
+from elotl import corpus as elotl_corpus
+# Código de ayudantía
+# def preprocess_corpus(corpus):
+#     # Obtener la oración de L1,
+#     # quitar signos de puntuación y
+#     # obtiene la lista de palabras
+#     word_list_l1 = []
+#     word_list_l2 = []
+#     for row in corpus:
+#         word_list_l1.extend(extract_words_from_sentence(row[0]))
+#     # Obtener la oración de L1,
+#     # quitar signos de puntuación y
+#     # obtiene la lista de palabras
+#         word_list_l2.extend(extract_words_from_sentence(row[1]))
+#     return word_list_l1, word_list_l2
+
+
+
+def sum_dicts(dic1: dict, dic2: dict) -> dict:
+    return {k: dic1.get(k, 0) + dic2.get(k, 0) for k in set(dic1) | set(dic2)}
+
+def sort_dict(dic: dict, max_to_min=True) -> dict:
+    return {k: v for k, v in sorted(dic.items(), key=lambda item: item[1], reverse = max_to_min)}
+
+def extract_chars_from_sentence(sentence, spaces=False, numbers = False):
+    chars = {}
+    for word in sub(r'[^\w\s\']', ' ', sentence).lower().split():
+        w = Counter(word)
+        chars = sum_dicts(chars,w)
+    return chars
+
+def chars_from_corpus(corpus, spaces=False, numbers = False) -> dict:
+    chars = {}
+    for sentence in corpus:
+        chars = sum_dicts(sentence, chars)
+    return chars
+
+# Función modificada de ayudantía
+def preprocess_corpus(corpus):
+    # Obtener la oración de L1,
+    # quitar signos de puntuación y
+    # obtiene una cuenta de caracteres
+    char_dict_l1 = {}
+    char_dict_l2 = {}
+    for row in corpus:
+        char_dict_l1 = sum_dicts(extract_chars_from_sentence(row[0]), char_dict_l1)
+    # Obtener la oración de L1,
+    # quitar signos de puntuación y
+    # obtiene la una cuenta de caracteres
+        char_dict_l2 = sum_dicts(extract_chars_from_sentence(row[0]), char_dict_l2)
+    return char_dict_l1, char_dict_l2
+
+# Importar corpus de nahúatl, otomí y español
+axolotl = elotl_corpus.load("axolotl")
+tsunkua = elotl_corpus.load("tsunkua")
+# Pre procesamiento de corpus
+spanish_char_na, nahuatl_char = preprocess_corpus(axolotl)
+spanish_char_oto, otomi_char = preprocess_corpus(tsunkua)
+D = sort_dict(nahuatl_char)
+plt.plot(range(len(D)), list(D.values()))
+plt.xticks(range(len(D)), list(D.keys()))
+plt.show()
+# def extract_words_from_sentence(sentence: str) -> list:
+#     return sub(r'[^\w\s\']', ' ', sentence).lower().split()
+
+# spanish_words_na, nahuatl_words = preprocess_corpus(axolotl)
+# spanish_words_oto, otomi_words = preprocess_corpus(tsunkua)
+
+# nahuatl_vocabulary = Counter(nahuatl_words)
+# nahuatl_es_vocabulary = Counter(spanish_words_na)
+# otomi_vocabulary = Counter(otomi_words)
+# otomi_es_vocabulary = Counter(spanish_words_oto)
+
+# Importar n-gramas de caracteres n=2
+# Respuestas stopwords coinciden con las paabras más comunes en Zipf?
+# Realizar nube de palabras sans stopwords et palabras de zipf
+
+# Lenguaje aleatorio con distribuciones Poisson, Uniforme Normal con media en cero, bi/tri normal, log normal, Zipf, 
+# Usemos enlace egalitario para generar un nodo de tipo "letra" y cada rama es una palabra
